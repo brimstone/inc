b5b4f961d852a4b0fac77ec1bcd598e3f93d38a1
Matt Robinson <brimstone@the.narro.ws>
2021-08-14 14:02:36 -0400

    incd/serve: Actually make it serve something

--- cmd/incd/cmd/serve.go
+++ cmd/incd/cmd/serve.go
@@ -15,8 +15,14 @@
 package cmd
 
 import (
-	"fmt"
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/brimstone/logger"
 	"github.com/spf13/cobra"
 )
 
@@ -26,8 +32,72 @@ func ServeCmd() *cobra.Command {
 		Use:   "serve",
 		Short: "Start an Inc server",
 		Long:  `Starts an Inc server on a TCP port.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("serve called")
+		RunE: func(cmd *cobra.Command, args []string) error {
+			log := logger.New()
+
+			ctx, allDone := context.WithCancel(context.Background())
+
+			s := &http.Server{
+				Addr:           ":8080", // TODO make this a flag
+				ReadTimeout:    60 * time.Second,
+				WriteTimeout:   60 * time.Second,
+				MaxHeaderBytes: 1 << 20,
+			}
+			go func() {
+				log.Info("Starting http server")
+				err := s.ListenAndServe()
+				if err != nil && err != http.ErrServerClosed {
+					log.Error("http server closed",
+						log.Field("err", err.Error()),
+					)
+				}
+			}()
+
+			go func() {
+				// Set up channel on which to send signal notifications.
+				// We must use a buffered channel or risk missing the signal
+				// if we're not ready to receive when the signal is sent.
+				c := make(chan os.Signal, 1)
+				signal.Notify(c, os.Interrupt)
+
+				// Block until a signal is received.
+				s := <-c
+				log.Info("Got signal",
+					log.Field("signal", s),
+				)
+				allDone()
+			}()
+
+			me, _ := os.Executable()
+			fi, _ := os.Stat(me)
+			origTime := fi.ModTime()
+			needReload := false
+			log.Info("Waiting for executable to change")
+			for ctx.Err() == nil {
+				fi, err := os.Stat(me)
+				if err != nil {
+					continue
+				}
+				if origTime != fi.ModTime() {
+					needReload = true
+					allDone()
+					log.Warn("Detected change in executable, relaunching")
+					break
+				}
+				select {
+				case <-time.After(time.Second):
+				case <-ctx.Done():
+					break
+				}
+			}
+			s.Shutdown(context.Background())
+
+			if needReload {
+				if err := syscall.Exec(me, os.Args, os.Environ()); err != nil {
+					log.Fatal(err)
+				}
+			}
+			return nil
 		},
 	}
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,20 @@ go 1.13
 
 require (
 	github.com/blang/semver v3.5.1+incompatible
+	github.com/brimstone/logger v0.0.0-20210414185108-9babd7891711
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/rhysd/go-github-selfupdate v1.2.1
-	github.com/spf13/cobra v0.0.5
-	github.com/spf13/viper v1.6.2
+	github.com/rhysd/go-github-selfupdate v1.2.3
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/cast v1.4.0 // indirect
+	github.com/spf13/cobra v1.2.1
+	github.com/spf13/viper v1.8.1
+	github.com/ulikunitz/xz v0.5.10 // indirect
+	golang.org/x/crypto v0.0.0-20210813211128-0a44fdfbc16e // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
